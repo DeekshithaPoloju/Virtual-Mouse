c8fb2017582085dca27d2aca87861db81c156414
Deekshitha Poloju <deekshithapoloju18@gmail.com>
2026-09-22 10:33:05 +0530

    Initial Commit

--- hand_detector.py
+++ hand_detector.py
@@ -31,4 +31,5 @@ class AirHandDetector:
         x2, y2 = p2[0], p2[1]
 
         return math.hypot(x2 - x1, y2 - y1)
+        asdf kkjshdf kjsdfh kdsj akdfjh DAFKHDF KDHSF KHSDFKAJH KJDSFH KJASDFH 
     
